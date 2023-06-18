2f281f2886ab892ffd39675b3aeb12f33c347575
n-freman <smiron1703@gmail.com>
2023-06-18 09:07:56 +0500

    add auth classes

---
+++ src/domain/auth.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import TypeAlias
+
+URL: TypeAlias = str
+
+
+@dataclass
+class User:
+    id: int
+    first_name: str
+    last_name: str
+    username: str
+    is_active: bool
+    is_verified: bool
+
+
+@dataclass
+class Profile:
+    user_id: int
+    photo: URL # url to photo
+    background_photo: URL # url to profile background photo
+    description: str
+    date_joined: datetime
+
+
+@dataclass
+class Follow:
+    follower_user_id: int
+    followee_user_id: int
+    date_created: datetime
